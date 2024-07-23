c1fa2425390ed39cf4be0499aafd56193dad673b
wajdi15 <wajdimn77@gmail.com>
2024-07-23 03:11:53 +0200

    Fix some bugs on player_ball_assinger

--- player_ball_assigner.py/__init__.py
+++
@@ -1,1 +0,0 @@
-from .player_ball_assinger import PlayerBallAssigner
--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -1,3 +1,5 @@
+import math
+
 def get_center_of_bbox(bbox):
     x1,y1,x2,y2 = bbox
     return int((x1+x2)/2), int((y1+y2)/2)
@@ -6,4 +8,4 @@ def get_bbox_width(bbox):
     return bbox[2]-bbox[0]
 
 def measure_dist(p1,p2) :
-    return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
