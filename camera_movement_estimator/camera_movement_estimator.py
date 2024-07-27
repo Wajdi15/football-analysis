92c9cfeeae1c803f66d3df9ec67745f8aada6cad
wajdi15 <wajdimn77@gmail.com>
2024-07-27 02:06:07 +0200

    create camera movement estimator class

---
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -0,0 +1,66 @@
+import pickle
+import cv2
+import numpy as np
+import os
+import sys
+sys.path.append('../')
+from utils import measure_dist
+
+class CameraMovementEstimator() :
+    def __init__(self,frame) :
+        self.minimum_dist = 5
+
+        self.lk_params = dict(
+            winSize = (15,15),
+            maxLevel = 2,
+            criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,10,0.03)
+        )
+
+        first_frame_grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        mask_features = np.zeros_like(first_frame_grayscale)
+        mask_features[:,0:20] = 1
+        mask_features[:,900:1050] = 1
+
+        self.features = dict(
+            maxCorners = 100,
+            qualityLevel = 0.3,
+            minDistance = 3,
+            blockSize = 7,
+            mask = mask_features
+        )
+ 
+    
+    def get_camera_movement(self,frames,read_from_stub=False,stub_path=None) :
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path,'rb') as f:
+                return pickle.load(f)
+            
+        camera_movement = [[0,0]*len(frames)]
+        old_gray =  cv2.cvtColor(frames[0],cv2.COLOR_BGR2GRAY)
+        old_features = cv2.goodFeaturesToTrack(old_gray,**self.features) #detect good corners to track in an image get the x , y
+
+        for frame_num in range(1,len(frames)) :
+            frame_gray = cv2.cvtColor(frames[frame_num],cv2.COLOR_BGR2GRAY)
+            new_features, _,_= cv2.calcOpticalFlowPyrLK(old_gray,frame_gray,old_features,None,**self.lk_params) #Tracks Motion
+
+            max_dist = 0
+            camera_movement_x,camera_movement_y = 0,0
+
+            for i , (new,old) in enumerate(new_features,old_features) :
+                new_features_point = new.ravel()
+                old_features_point = old.ravel() 
+                dist = measure_dist(new_features_point,old_features_point)
+                if dist > max_dist : 
+                    max_dist = dist
+                    camera_movement_x =  new_features_point[0] - old_features_point[0]
+                    camera_movement_y =  new_features_point[1] - old_features_point[1]
+            if  max_dist > self.minimum_dist :
+                camera_movement[frame_num] = [camera_movement_x,camera_movement_y]
+                old_features = cv2.goodFeaturesToTrack(frame_gray,**self.features)
+            old_gray = frame_gray.copy()
+        
+        if stub_path is not None:
+            with open(stub_path,'wb') as f:
+                pickle.dump(camera_movement,f)
+        
+        return camera_movement
